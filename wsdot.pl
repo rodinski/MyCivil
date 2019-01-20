4428b441d587fc8db6d00839cab6535aac993e75
rodinski <crimson.blue.2@gmail.com>
2019-01-20 15:27:37 -0600

    added a diagram and logic to deal with gap need when writing the next bar spaceing for the right hand side

--- wsdot.pl
+++ wsdot.pl
@@ -27,9 +27,12 @@ foreach my $memb ( 1 .. $end_span ) {
     my $end_bar = scalar @b;
 
     foreach my $i ( 1 .. $end_bar ) {
-
         my $dist_f_end = $b[ $i - 1 ][0];
-        if ($i != $end_bar) { my $something   = $b[ $i ][0] - $b[ $i - 1 ][0]} ;
+
+        my $next_gap   =  0;
+        # note $i-1  is the current information
+        if ($i != $end_bar) { $next_gap   = $b[ $i ][0] - $b[ $i - 1 ][0]} ;
+
         my $sp         = $b[ $i - 1 ][1];
         my $area       = $b[ $i - 1 ][2];
         my $Sx         = $b[ $i - 1 ][3];
@@ -38,10 +41,17 @@ foreach my $memb ( 1 .. $end_span ) {
         my $lf_bm_end = $HoA{pier};    #assume pier
         my $rt_bm_end = $HoA{pier};    #assume pier
         for my $end ( "L", "R" ) {
-            if ( $memb == 1 && $end eq "L" )         { $lf_bm_end = $HoA{end} + $HoA{e}; }
-            if ( $memb == $end_span && $end eq "R" ) { $rt_bm_end = -$HoA{end} + $HoA{e}; }
+            if ( $memb == 1  )         { $lf_bm_end = $HoA{end} + $HoA{e}; }
+            if ( $memb == $end_span  ) { $rt_bm_end = +$HoA{end} + $HoA{e}; }
             # now I have end of beam locastion relative to all support points
-
+            #
+            #      |____________bm_length______________|    = -lf_bm_end +s +rt_bm_end
+            #         ^              s                    ^
+            #           |____________bm_length______________|    = -lf_bm_end +s +rt_bm_end
+            #         ^              s                        ^
+            #           |____________bm_length______________|    = -lf_bm_end +s +rt_bm_end
+            #         ^              s                    ^
+            my $bm_length = -$lf_bm_end +$s*12 -$rt_bm_end;   #bars mirro about $bm_length not $s
             if ( $end eq "R" && $i == $end_bar ) { next }    #skip second version of last spacing
                  # it doesn't get reported
 
@@ -50,13 +60,29 @@ foreach my $memb ( 1 .. $end_span ) {
             if ( $end eq "L" ) { $l = $lf_bm_end + $dist_f_end }
             if ( $end eq "R" ) { $l = $s * 12 - $rt_bm_end - $dist_f_end }
 
+            if ( $end eq "R" ) { 
+                    # first find where it is on $bm_length only
+                    # because we come from the left side only subtract the  gap
+                    #  ( sym ) less next_gap
+                    my   $bm_l = ($bm_length - $dist_f_end )-$next_gap ;
+                    $l = $bm_l + $lf_bm_end;
+            }
+
+
+
             if ( $end eq "x" ) { print "==  $sp   $lf_bm_end   $dist_f_end  $l \n"; }
             if ( $end eq "x" ) { my $tmp_s = $s * 12.0; print "==  $sp   $rt_bm_end   $dist_f_end  $tmp_s   $l \n"; }
 
 #            if ( $end eq "L" && $l < 0 ) { @sec = (); $l   = 0.0; }    # when $l is neg empty @sec
 #            if ( $end eq "L" && scalar @sec == 0 ) { $l = 0.0; }    # first dist must be 0.0
-            push @line, sprintf "%3d %6.2f  L    %5.2f  %6.2f  %5.2f\n",
-              , $memb, $l / 12., $sp, $area, $Sx;
+            my $tmp_line="";
+            if ($l >= $s*12.0 ) { $tmp_line = "*" }  #did we go beyond $s? 
+            $tmp_line .= sprintf "%3d %6.2f  L    %5.2f  %6.2f  %5.2f\n",
+                                  , $memb, $l / 12., $sp, $area, $Sx;
+
+            push @line, $tmp_line;
+        
+            #print "$lf_bm_end    $s     $rt_bm_end     ".  $bm_length/12.0. "\n";
 
         }
         my $insert = ( scalar @sec ) / 2  ;
@@ -71,12 +97,12 @@ foreach my $memb ( 1 .. $end_span ) {
 
       if ($j == 0 && $val !~ /-/ ) { #is the fisrt DIST > 0 
         my @f = split /\s+/,$val ;
-        if ($f[2]>0) {push @clean, ( "#".$val) }
+        if ($f[2]>0) {push @clean, ( "*".$val) }
         $val = "0.0me".$val;
       }
 
       if ($val =~ /-/ ) {  
-        push @clean, ( "#".$val ) ;
+        push @clean, ( "*".$val ) ;
         if ( $sec[$j+1] !~ /-/ ) { push @clean, ( "0.0me".$val) }
       }
 
