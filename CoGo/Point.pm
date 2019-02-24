050db7e2c3a54c8f0eb062494b1040c724ca119d
rodinski <crimson.blue.2@gmail.com>
2019-02-24 07:30:14 -0600

    added Point.pm and Ray.pm   with Moo

--- CoGo/Point.pm
+++ CoGo/Point.pm
@@ -1,73 +1,52 @@
 package Point;
 use Moo;
 use Type::Tiny;
+use Types::Standard qw( Str Int ArrayRef HashRef );
 use Scalar::Util qw( looks_like_number);
-
-# some test that Type::Tiny will do for us
-my $NUM_TEST = "Type::Tiny"->new(
-    name       => "Number",
-    constraint => sub { looks_like_number($_)   },
-    message    => sub { "$_ ain't a number" },
-);
-
-has E => ( is => 'rw', required => 1, isa => $NUM_TEST, );
-has N => ( is => 'rw', required => 1, isa => $NUM_TEST, );
-has y => ( is => 'rw', required => 1, default => undef, );
-has x => ( is => 'rw', required => 0 );
-has ID =>( is => 'r', required => 0 ) ;
-has tags =>( is => 'rw', required => 0 ) ;
-
+use Carp qw( confess ); 
+#has Point => ( 
+#    is => 'rw'
+#  , required => 1
+#  , isa => sub {  confess "'$_[0]' is not a Hash!"
+#          unless  $_[0] =~/Point=HASH/ }
+#  );
+#
+has E =>     ( is => 'rw', required => 1, 
+               isa => sub {  confess "'$_[0]' is not a number!"
+                      unless looks_like_number $_[0]}
+               );
+has N =>     ( is => 'rw', required => 1,
+               isa => sub {  confess "'$_[0]' is not a number!"
+                      unless looks_like_number $_[0]}
+               );
+has ID =>    ( is => 'rw', required => 0 ) ;
+has tags =>  ( is => 'rw', required => 0 ) ;
 
 
 sub to_string {
     my ($self) = @_;
-    return sprintf "[%s, %s]", $self->x, $self->y;
+    return sprintf "[%s, %s]", $self->E, $self->N;
 }
 
 sub distance_from_origin {
     my ($self) = @_;
-    my $sqr = $self->x**2 + $self->y**2;
+    my $sqr = $self->E**2 + $self->N**2;
     return sqrt($sqr);
 }
 
 sub translate {
-    my ( $self, $dx, $dy ) = @_;
-
-    my $x_temp = $self->x;
-    $x_temp += $dx;
-    $self->x($x_temp);
-    $self->y( $self->y + $dy );
+    my ( $self, $dE, $dN ) = @_;
+    my $E_temp = $self->E;
+    $E_temp += $dE;
+    $self->E($E_temp);
+    $self->N( $self->N + $dN );
     return;
 }
 
 sub bearing_from_N {
     my ($self) = @_;
-    my $bearing_from_N = atan2( $self->y, $self->x );
+    my $bearing_from_N = atan2( $self->N, $self->E );
     return $bearing_from_N;
 }
 
-
-
-
-package Bearing;
-use Moo;
-use Math::Trig qw( :pi rad2deg );
-use Type::Tiny;
-use Scalar::Util qw( looks_like_number);
-
-my $BEARING_TEST = "Type::Tiny"->new(
-    name       => "Number",
-    constraint => sub { looks_like_number($_)  && -pi < $_ && $_<= pi },
-    message    => sub { "$_ ain't a number or isn't b/w -pi to pi" },
-);
-has bearing => ( 
-    is => 'rw', 
-    required => 1, 
-    isa => $BEARING_TEST 
-);
-
-sub b   { my ($self) = @_; return $self->bearing; } 
-sub deg { my ($self) = @_; return rad2deg $self->bearing }
-
 1;
-
--- CoGo/Ray.pm
+++ CoGo/Ray.pm
@@ -1,23 +1,31 @@
 package Ray;
-use Point;
+use My::CoGo::Bearing;
+use My::CoGo::Point;
 use Moo;
 
+sub obj_test { 
+  my($invocant,$class) = @_;
+  my $whoami = Scalar::Util::blessed($invocant);
+  die "$_[0] is " . ref $_[0] unless ( $whoami eq $class);
+}
 has 'name' => (
     is       => 'ro',
     required => 1,
 );
-has 'point' => (
+has point => (
     is  => 'ro',
     isa => sub {
         die "$_[0] is " . ref $_[0] unless (1);
     },
 );
 
-has 'bearing' => (
+has bearing => (
     is  => 'ro',
     isa => sub {
         die "$_[0] is " . ref $_[0] unless (1);
     },
 );
+
+
 1;
 
