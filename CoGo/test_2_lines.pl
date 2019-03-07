9294efcda4322c39f5f124f212ea65fd79fc50cb
rodinski <crimson.blue.2@gmail.com>
2019-03-06 21:38:14 -0600

    2_lines subrouting now working

--- CoGo/test_2_lines.pl
+++ CoGo/test_2_lines.pl
@@ -23,21 +23,27 @@ foreach my $i ( 0 .. 11) {
 my $sa = Segment->new( pa => $clock[0] , pb => $clock[4] );
 my $sb = Segment->new( pa => $clock[9] , pb => $clock[5] );
 
-intersect ( $sa, $sb );
+my $int_pt = intersect ( $sa, $sb );
+say Dumper $int_pt;
 
 sub intersect {
     my $seg_1 = shift;
     my $seg_2 = shift;
     #say Dumper $seg_1;
+    #
+       
+
     my $d_N = $seg_2->pa->N - $seg_1->pa->N ;
     my $d_M = $seg_1->bearing - $seg_2->bearing ;
-
+    if ( $d_M == 0 ) { return "Lines are parallel, no cross point"; }
     my $t1 =  $seg_1->bearing * $seg_1->pa->E ;
     my $t2 =  $seg_2->bearing * $seg_2->pa->E ;
 
 
-    my $x = ( $d_N - $t2 + $t1 ) /  $d_M ;  
-    my $y = (     $seg_1->bearing * $x - $seg_1->pa->E) + $seg_1->pa->N ;
-    my $y_chk = ( $seg_2->bearing * $x - $seg_2->pa->E) + $seg_2->pa->N ;
-    say "$x\t $y \t $y_chk";
+    my $x = ( $d_N + $t1 - $t2 ) /  $d_M ;  
+    my $y =      $seg_1->bearing * ($x - $seg_1->pa->E) + $seg_1->pa->N ;
+    my $y_chk =  $seg_2->bearing * ($x - $seg_2->pa->E) + $seg_2->pa->N ;
+    #say "$x\t $y \t $y_chk";
+    my $new_pt = Point->new( N => $y, E => $x ) ;
+    return $new_pt;
     }
